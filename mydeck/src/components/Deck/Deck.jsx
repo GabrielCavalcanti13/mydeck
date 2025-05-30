7e67b55f1f03b579a2aac18ed0202a94dae7dcdd
Gabriel Cavalcanti <gabrielcavalcanti1310@gmail.com>
2025-05-29 21:53:43 -0300

    swiper install

--- mydeck/src/components/Deck/Deck.jsx
+++ mydeck/src/components/Deck/Deck.jsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { getDecks } from "../../services/deckService";
 import Card from "../Card/Card";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
+
+import "swiper/swiper-bundle.css"; // CSS correto para Swiper 9
+
 import "./Deck.css";
 
 const Deck = () => {
@@ -27,16 +33,31 @@ const Deck = () => {
         <h2>{deck?.name?.name || deck?.name}</h2>
         <button onClick={() => navigate(-1)}>Voltar</button>
       </div>
-      <div className="card-list">
-        {deck?.cards?.map((card, index) => (
-          <Card
-            key={index}
-            name={card.name}
-            image={card.image}
-            attributes={deck.attributes}
-            values={card.values}
-          />
-        ))}
+
+      <div className="card-carousel">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          navigation
+          pagination={{ clickable: true }}
+          spaceBetween={20}
+          slidesPerView={1}
+          breakpoints={{
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+        >
+          {deck?.cards?.map((card, index) => (
+            <SwiperSlide key={index}>
+              <Card
+                name={card.name}
+                image={card.image}
+                attributes={deck.attributes}
+                values={card.values}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );

fd9d0f5f1ab2135300d16f50d0928a9d303213d6
Gabriel Cavalcanti <gabrielcavalcanti1310@gmail.com>
2025-05-29 21:22:04 -0300

    Deck style

--- mydeck/src/components/Deck/Deck.jsx
+++ mydeck/src/components/Deck/Deck.jsx
@@ -1,41 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { getDecks} from "../../services/deckService";
+import { getDecks } from "../../services/deckService";
 import Card from "../Card/Card";
 import "./Deck.css";
 
 const Deck = () => {
-    const { deckId } = useParams();
-    const navigate = useNavigate();
-    const [deck, setDeck] = useState(null);
-    
-    useEffect(() => {
-        const fetchDeck = async () => {
-          const decks = await getDecks();
-          const selectedDeck = decks.find((d) => d.id === deckId);
-          if (selectedDeck) {
-            setDeck(selectedDeck);
-          }
-        };
-    
-        if (deckId) fetchDeck();
-      }, [deckId]);
-  
-    return (
-        <div>
-					<h2>{deck?.name.name || deck?.name}</h2>
-					<button onClick={() => navigate(-1)}>Voltar</button>
-					{deck?.cards?.map((card, index) => (
-					<Card
-					key={index}
-					name={card.name}
-					image={card.image}
-					attributes={deck.attributes}
-					values={card.values}
-					/>
+  const { deckId } = useParams();
+  const navigate = useNavigate();
+  const [deck, setDeck] = useState(null);
+
+  useEffect(() => {
+    const fetchDeck = async () => {
+      const decks = await getDecks();
+      const selectedDeck = decks.find((d) => d.id === deckId);
+      if (selectedDeck) {
+        setDeck(selectedDeck);
+      }
+    };
+
+    if (deckId) fetchDeck();
+  }, [deckId]);
+
+  return (
+    <div className="deck-container">
+      <div className="deck-header">
+        <h2>{deck?.name?.name || deck?.name}</h2>
+        <button onClick={() => navigate(-1)}>Voltar</button>
+      </div>
+      <div className="card-list">
+        {deck?.cards?.map((card, index) => (
+          <Card
+            key={index}
+            name={card.name}
+            image={card.image}
+            attributes={deck.attributes}
+            values={card.values}
+          />
         ))}
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Deck;
